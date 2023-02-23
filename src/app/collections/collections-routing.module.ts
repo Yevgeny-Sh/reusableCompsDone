32d1cf4aa598cf568145bbf85188eac4bf0907dc
yevgeny <yevgeny.shtraiher@highcon.net>
2023-02-23 12:06:15 +0200

    implemented child routing in collections

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionsHomeComponent } from '../collections/collections-home/collections-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
-const routes: Routes = [{path:'',component:CollectionsHomeComponent}];
+const routes: Routes = [ {path:'',component:CollectionsHomeComponent,children:[
+  {path:'',component:BiographyComponent},{path:'companies',component:CompaniesComponent},{path:'Partners',component:PartnersComponent}
+]}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { PartnersComponent } from './partners/partners.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { BiographyComponent } from './biography/biography.component';
 
 
 @NgModule({
   declarations: [
-    CollectionsHomeComponent
+    CollectionsHomeComponent,
+    PartnersComponent,
+    CompaniesComponent,
+    BiographyComponent
   ],
   imports: [
     CommonModule,
